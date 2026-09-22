673c8659b95a5d79e8097582c90604710cb2b23f
BERNIER Ethan <ethan.bernier66@gmail.com>
2026-09-22 15:25:57 +0200

    Refactor: Methode setup ajouté avec @BeforeEach pour instancier un calculator nettoyer avant chaque test

--- src/main/java/com/EthanBernier/calculator/Calculator.java
+++ src/main/java/com/EthanBernier/calculator/Calculator.java
@@ -1,4 +1,14 @@
 package com.EthanBernier.calculator;
 
 public class Calculator {
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int divide(int a, int b) {
+        return a/b;
+    }
+
+
+
 }
--- src/test/java/com/EthanBernier/calculator/CalculatorTest.java
+++ src/test/java/com/EthanBernier/calculator/CalculatorTest.java
@@ -1,4 +1,46 @@
 package com.EthanBernier.calculator;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.*;
+
+
 public class CalculatorTest {
+    private Calculator calculator;
+
+    @BeforeEach
+    public void setUp() {
+         this.calculator = new Calculator();
+    }
+
+    @Test
+    public void devrait_retourner_la_somme_de_deux_entiers_positif(){
+
+        //GIVEN
+        int a = 5;
+        int b = 5;
+
+        //WHEN
+        int resultat = calculator.add(a,b);
+
+        //THEN
+        assertThat(resultat).isEqualTo(10);
+
+    }
+
+    @Test
+    public void devrait_retourner_le_quotient_de_deux_entiers_positif(){
+
+        //GIVEN
+        int a = 5;
+        int b = 5;
+
+        //WHEN
+        int resultat = calculator.divide(a,b);
+
+        //THEN
+        assertThat(resultat).isEqualTo(1);
+
+    }
+
 }
