d14cbab52d82872078e1a622054be2390a92d830
BERNIER Ethan <ethan.bernier66@gmail.com>
2026-09-22 16:36:46 +0200

    @ParameterizedTest avec csv

--- src/test/java/com/EthanBernier/calculator/CalculatorTest.java
+++ src/test/java/com/EthanBernier/calculator/CalculatorTest.java
@@ -3,23 +3,31 @@ package com.EthanBernier.calculator;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
 import static org.assertj.core.api.Assertions.*;
 
 
 public class CalculatorTest {
 
-    @Test
-    public void devrait_retourner_la_somme_de_deux_entiers_positif(){
+    @ParameterizedTest
+    @CsvSource({
+            "0, 1, 1",
+            "1, 2, 3",
+            "-2, 2, 0",
+            "0, 0, 0",
+            "-1, -2, -3"
+    })
+    public void devrait_retourner_la_somme_de_deux_entiers_positif(int a, int b, int result) {
 
         //GIVEN
-        int a = 5;
-        int b = 5;
 
         //WHEN
         int resultat = Calculator.add(a,b);
 
         //THEN
-        assertThat(resultat).isEqualTo(10);
+        assertThat(resultat).isEqualTo(result);
 
     }
 
